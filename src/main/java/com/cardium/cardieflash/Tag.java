662973a3c45637d571813be13b22d878df8d47ff
Necatican Yıldırım <necatican3344@gmail.com>
2019-01-13 21:47:32 +0300

    Created classes for Tags and TempDecks.

---
+++ src/main/java/com/cardium/cardieflash/Tag.java
@@ -0,0 +1,20 @@
+package com.cardium.cardieflash;
+
+public class Tag {
+
+    private int tagId;
+    private String name;
+
+    public Tag(int id, String name) {
+        this.tagId = id;
+        this.name = name;
+    }
+
+    public int gettagId() {
+        return tagId;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/com/cardium/cardieflash/TemporaryDeck.java
@@ -0,0 +1,60 @@
+package com.cardium.cardieflash;
+
+import java.util.HashMap;
+
+public class TemporaryDeck {
+    private String name;
+    private HashMap<Integer, Card> cards;
+
+
+    public TemporaryDeck(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addCards(HashMap<Integer, Card> cards)
+    {
+        this.cards = cards;
+    }
+
+    public HashMap<Integer, Card> getCards() {
+        return cards;
+    }
+
+    public boolean addCard(Card card) {
+        if (cards.containsKey(card.getCid())) {
+            return false;
+        } else {
+            cards.put(card.getCid(), card);
+            return true;
+
+        }
+    }
+
+    public boolean removeCard(int cid) {
+        if (!cards.containsKey(cid)) {
+            return false;
+        } else {
+            cards.remove(cid);
+            return true;
+        }
+    }
+
+    public boolean removeCard(Card card) {
+        return removeCard(card.getCid());
+    }
+
+    public boolean editCard(Card card) {
+        if (!cards.containsKey(card.getCid())) {
+            return false;
+        } else {
+            cards.put(card.getCid(), card);
+            return true;
+        }
+
+    }
+
+}
